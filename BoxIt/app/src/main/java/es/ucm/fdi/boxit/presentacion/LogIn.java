5b102301f86df8eb74a3feb89ac222b44be6dbc6
imclau22 <claudi-4@hotmail.es>
2023-11-05 19:22:51 +0100

    login (front)

--- BoxIt/app/src/main/java/es/ucm/fdi/boxit/presentacion/LogIn.java
+++ BoxIt/app/src/main/java/es/ucm/fdi/boxit/presentacion/LogIn.java
@@ -2,16 +2,19 @@ package es.ucm.fdi.boxit.presentacion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import es.ucm.fdi.boxit.R;
 
-public class MainActivity extends AppCompatActivity {
+public class LogIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_log_in);
+
+
+
     }
 }
