bca310c8484f10f3f34c3fd1e5cc47e2043050f0
imclau22 <claudi-4@hotmail.es>
2023-11-10 12:43:47 +0100

    formulario de registrio front

--- BoxIt/app/src/main/java/es/ucm/fdi/boxit/presentacion/LogIn.java
+++ BoxIt/app/src/main/java/es/ucm/fdi/boxit/presentacion/LogIn.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textview.MaterialTextView;
 
 import es.ucm.fdi.boxit.R;
 
@@ -14,7 +19,18 @@ public class LogIn extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
+        //NO FUCIONA EL ONCLICK DE REGISTRARSE
+        TextView registrarse = findViewById(R.id.registrateBtn);
+
+        registrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogIn.this, Registro.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
+
 }
---
+++ BoxIt/app/src/main/java/es/ucm/fdi/boxit/presentacion/Registro.java
@@ -0,0 +1,16 @@
+package es.ucm.fdi.boxit.presentacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import es.ucm.fdi.boxit.R;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
