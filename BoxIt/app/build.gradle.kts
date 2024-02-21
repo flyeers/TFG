88ce68146f51d4f39eb263f4ae0dc6353dbf7649
flyeers <juliafluper@gmail.com>
2024-02-21 18:03:05 +0100

    spoty prep creo + otars cosas

--- BoxIt/app/build.gradle.kts
+++ BoxIt/app/build.gradle.kts
@@ -43,11 +43,14 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation ("com.github.bumptech.glide:glide:4.13.0")
+    implementation(files("libs/spotify-app-remote-release-0.8.0.arr"))
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.google.code.gson:gson:2.6.1")
 
-   
+
+
 
 }
