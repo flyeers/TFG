e28a5bd524bcacf3c45f569ed5193c6a7a21c4eb
imclau22 <claudi-4@hotmail.es>
2024-03-13 20:34:59 +0100

    Update build.gradle.kts

--- BoxIt/app/build.gradle.kts
+++ BoxIt/app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    implementation("com.google.firebase:firebase-messaging:23.3.1")
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
 
 
